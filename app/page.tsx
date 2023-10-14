107723f9d6e426aee5e3d95ec531aa1aa35a8b0b
engrjeff <engineerjeff@gmail.com>
2023-10-14 11:28:42 +0800

    feat: add theme toggler

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggler } from "@/components/theme-toggler";
+
 export default function Home() {
   return (
     <main>
       <h1>Church Finder PH</h1>
+      <ThemeToggler />
     </main>
   );
 }
